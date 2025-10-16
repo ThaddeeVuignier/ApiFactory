d066768e1bc6215a20d69f8d3e7ae5acb35e5d98
Thaddee <thaddee.vuignier@gmail.com>
2025-10-16 11:23:10 +0200

    feat(init): initialize hexagonal Spring Boot backend with Docker and PostgreSQL

---
+++ src/test/java/com/pharma/apifactory/ApiFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharma.apifactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
